704ab64020c638057de30841f6dcc2f0d0f636fd
Cleanderson Lobo <cleandersonlobo@gmail.com>
2021-04-09 14:29:11 -0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,10 @@
-// module.exports = function (api) {
-// 	api.cache(true);
+module.exports =  {
 
-// 	const isProduction =
-// 		process.env.NODE_ENV === 'production' ||
-// 		process.env.BABEL_ENV === 'production';
+	let presets = ['module:metro-react-native-babel-preset'];
+	let plugins = [];
 
-// 	const isTest = process.env.NODE_ENV === 'test';
-
-// 	let presets = ['module:metro-react-native-babel-preset'];
-// 	let plugins = [];
-
-// 	if (isProduction || isTest) {
-// 		plugins.push('transform-remove-console');
-// 	}
-
-// 	return {
-// 		presets,
-// 		plugins,
-// 	};
-// };
+	return {
+		presets,
+		plugins,
+	};
+};

1148bfdce43405cb7bdecb2d7515d720737d6b88
Cleanderson Lobo <cleandersonlobo@gmail.com>
2021-04-09 14:32:09 -0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,4 @@
 module.exports =  {
-
-	let presets = ['module:metro-react-native-babel-preset'];
-	let plugins = [];
-
-	return {
-		presets,
-		plugins,
-	};
+		presets: ['module:metro-react-native-babel-preset'],
+		plugins: [],
 };
